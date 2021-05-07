512b51c3bada1cd135863be1bc3477da91b85771
Faris AlHarbi <36749108+ARTiSticov@users.noreply.github.com>
2021-05-07 04:48:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -151,11 +151,7 @@ clf = get_classifier(classifier_name, params)
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state=1234)
 
-# Feature scaling
-from sklearn.preprocessing import StandardScaler
-sc = StandardScaler()
-X_train = sc.fit_transform(X_train)
-X_test = sc.transform(X_test)
+
 
 #Training the model on the training set :
 clf.fit(X_train, y_train)
